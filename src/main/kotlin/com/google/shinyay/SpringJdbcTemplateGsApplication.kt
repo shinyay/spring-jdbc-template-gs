568c8955da6e50637fae18cc127fcd680526d82c
shinyay <shinya.com@gmail.com>
2020-08-07 22:27:44 +0900

    Add create table statement

--- src/main/kotlin/com/google/shinyay/SpringJdbcTemplateGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringJdbcTemplateGsApplication.kt
@@ -2,11 +2,24 @@ package com.google.shinyay
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-class SpringJdbcTemplateGsApplication
+class SpringJdbcTemplateGsApplication(val jdbcTemplate: JdbcTemplate) : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		logger.info("Creating Tables...")
+		jdbcTemplate.execute("DROP TABLE customers IF EXISTS")
+		jdbcTemplate.execute("""
+					CREATE TABLE customers(
+						id SERIAL,
+						first_name VARCHAR(255),
+						last_name VARCHAR(255)
+		""".trimIndent())
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringJdbcTemplateGsApplication>(*args)
